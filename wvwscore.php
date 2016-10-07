aced529a5f713bb2bb702eb65260ee1343bde584
Jirayu <me@jirayu.in.th>
2016-10-08 01:12:20 +0700

    crude template system

--- wvwscore.php
+++ wvwscore.php
@@ -53,13 +53,35 @@ if(isset($options['match'])){
 	$matches = explode(",", $options['match']);
 }
 
+/**
+ * Load Template
+ */
+
+$use_template = false;
+if(isset($options['template'])){
+	$use_template = true;
+	$template = explode("<!-- START TIER LOOP -->", file_get_contents($options['template']));
+
+	$template_head = $template[0];
+
+	$template = explode("<!-- END TIER LOOP -->", $template[1]);
+
+	$template_body = $template[0];
+	$template_footer = $template[1];
+}
+
 $wvwscore_base = "https://api.guildwars2.com/v2/wvw/matches/";
+
+if($use_template){
+	echo $template_head;
+}
 
 foreach($matches as $match){
 	$score = json_decode(file_get_contents($wvwscore_base.$match, false, stream_context_create($arrContextOptions)));
-	
-	echo "---------------------------------------------------------------------\n";
-	echo " Match ID: ".$match."\n";
+	if(!$use_template){
+		echo "---------------------------------------------------------------------\n";
+		echo " Match ID: ".$match."\n";
+	}
 
 	$team['red']['host'] = $score->worlds->red;
 	$team['blue']['host'] = $score->worlds->blue;
@@ -106,26 +128,55 @@ foreach($matches as $match){
 		}
 		unset($guest);
 	}
+	
+	if($use_template){
+		$tier_num = explode("-", $match);
+		$red_team = $server_name[$team['red']['host']] ." ".$guest_name['red'];
+		$blue_team = $server_name[$team['blue']['host']] ." ".$guest_name['blue'];
+		$green_team = $server_name[$team['green']['host']] ." ".$guest_name['green'];
+		$output = $template_body;
+		$output = str_replace("<!-- TXT TIER_NUM -->", $tier_num[1],$output);
+		$output = str_replace("<!-- TXT TEAM_RED -->", $red_team, $output);
+		$output = str_replace("<!-- TXT TEAM_BLUE -->", $blue_team, $output);
+		$output = str_replace("<!-- TXT TEAM_GREEN -->", $green_team, $output);
+		$output = str_replace("<!-- TXT SCORE_RED -->", number_format($team['red']['scores']), $output);
+		$output = str_replace("<!-- TXT SCORE_BLUE -->", number_format($team['blue']['scores']), $output);
+		$output = str_replace("<!-- TXT SCORE_GREEN -->", number_format($team['green']['scores']), $output);
+		$output = str_replace("<!-- TXT KILL_RED -->", number_format($team['red']['kills']), $output);
+		$output = str_replace("<!-- TXT KILL_BLUE -->", number_format($team['blue']['kills']), $output);
+		$output = str_replace("<!-- TXT KILL_GREEN -->", number_format($team['green']['kills']), $output);
+		$output = str_replace("<!-- TXT DEATH_RED -->", number_format($team['red']['deaths']), $output);
+		$output = str_replace("<!-- TXT DEATH_BLUE -->", number_format($team['blue']['deaths']), $output);
+		$output = str_replace("<!-- TXT DEATH_GREEN -->", number_format($team['green']['deaths']), $output);
+		$output = str_replace("<!-- TXT RATIO_RED -->", @number_format(($team['red']['kills']/$team['red']['deaths']), 2, '.', ''), $output);
+		$output = str_replace("<!-- TXT RATIO_BLUE -->", @number_format(($team['blue']['kills']/$team['blue']['deaths']), 2, '.', ''), $output);
+		$output = str_replace("<!-- TXT RATIO_GREEN -->", @number_format(($team['green']['kills']/$team['green']['deaths']), 2, '.', ''), $output);
 
-	echo "   Red Team: ". $server_name[$team['red']['host']] ." ".$guest_name['red']."\n";
-	echo "    - Score: ". number_format($team['red']['scores'], "0", "", ",") ."\n";
-	echo "    - Kills: ". number_format($team['red']['kills'], "0", "", ",") ."\n";
-	echo "    - Deaths: ". number_format($team['red']['deaths'], "0", "", ",") ."\n";
-	echo "    - KDR: ". @number_format(($team['red']['kills']/$team['red']['deaths']), 2, '.', '') ."\n";
+		echo $output;
+	}else{
+		echo "   Red Team: ". $server_name[$team['red']['host']] ." ".$guest_name['red']."\n";
+		echo "    - Score: ". number_format($team['red']['scores'], "0", "", ",") ."\n";
+		echo "    - Kills: ". number_format($team['red']['kills'], "0", "", ",") ."\n";
+		echo "    - Deaths: ". number_format($team['red']['deaths'], "0", "", ",") ."\n";
+		echo "    - KDR: ". @number_format(($team['red']['kills']/$team['red']['deaths']), 2, '.', '') ."\n";
 
-	echo "   Blue Team: ". $server_name[$team['blue']['host']] ." ".$guest_name['blue']."\n";
-	echo "    - Score: ". number_format($team['blue']['scores'], "0", "", ",") ."\n";
-	echo "    - Kills: ". number_format($team['blue']['kills'], "0", "", ",") ."\n";
-	echo "    - Deaths: ". number_format($team['blue']['deaths'], "0", "", ",") ."\n";
-	echo "    - KDR: ". @number_format(($team['blue']['kills']/$team['blue']['deaths']), 2, '.', '') ."\n";
+		echo "   Blue Team: ". $server_name[$team['blue']['host']] ." ".$guest_name['blue']."\n";
+		echo "    - Score: ". number_format($team['blue']['scores'], "0", "", ",") ."\n";
+		echo "    - Kills: ". number_format($team['blue']['kills'], "0", "", ",") ."\n";
+		echo "    - Deaths: ". number_format($team['blue']['deaths'], "0", "", ",") ."\n";
+		echo "    - KDR: ". @number_format(($team['blue']['kills']/$team['blue']['deaths']), 2, '.', '') ."\n";
 
-	echo "   Green Team: ". $server_name[$team['green']['host']] ." ".$guest_name['green']."\n";
-	echo "    - Score: ". number_format($team['green']['scores'], "0", "", ",") ."\n";
-	echo "    - Kills: ". number_format($team['green']['kills'], "0", "", ",") ."\n";
-	echo "    - Deaths: ". number_format($team['green']['deaths'], "0", "", ",") ."\n";
-	echo "    - KDR: ". @number_format(($team['green']['kills']/$team['green']['deaths']), 2, '.', '') ."\n";
+		echo "   Green Team: ". $server_name[$team['green']['host']] ." ".$guest_name['green']."\n";
+		echo "    - Score: ". number_format($team['green']['scores'], "0", "", ",") ."\n";
+		echo "    - Kills: ". number_format($team['green']['kills'], "0", "", ",") ."\n";
+		echo "    - Deaths: ". number_format($team['green']['deaths'], "0", "", ",") ."\n";
+		echo "    - KDR: ". @number_format(($team['green']['kills']/$team['green']['deaths']), 2, '.', '') ."\n";
+	}
 
 	unset($guest_name);
 }
 
+if($use_template){
+	echo $template_footer;
+}
 ?>
